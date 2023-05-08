dfdcde9221fbb9dfe1daaf5fb2953c8232d58d12
Ravi Lour <ravijaat2917@gmail.com>
2023-05-08 11:32:40 +0530

    Install Raect App and Clear Unnecessary Code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="bg-dark">
+        <h1 className="text-success">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
