1b912e1957c71ea4a53547a908b4f78a711167d2
Ravi Lour <ravijaat2917@gmail.com>
2023-05-24 08:44:20 +0530

    app production build

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import connectDB from "./Config/db.js";
 import userRoutes from "./Routes/user.Routes.js";
 import adminRoutes from "./Routes/admin.Routes.js";
 import doctorRoutes from "./Routes/doctor.Routes.js";
+import path from 'path';
+import { fileURLToPath } from "url";
 
 // Rest object
 const app = express();
@@ -19,10 +21,19 @@ app.use(express.json());
 // Database Connection
 connectDB(process.env.DATABASE_URL);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(path.join(__dirname));
+app.use(express.static(path.join(__dirname,"./client/build")));
+
 // Routes
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/doctor", doctorRoutes);
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+});
 
 const PORT = process.env.PORT;
 // Listen Port
